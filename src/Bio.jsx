59161c0c6b7dc01f61fabcb1a1bf245e1a7cddae
XixoEcaP <franciscoecapinheiro@gmail.com>
2024-07-05 14:27:32 +0100

    Adding record player

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -6,7 +6,7 @@ const Bio = ({ handleEmailClick }) => {
         <div className="bio-container">
             <h1 id="title">Francisco Eça</h1>
             <img src={process.env.PUBLIC_URL + '/assets/profile.png'} alt="Profile" className="profile-photo" />
-            <p>🎸 By day, I excelled in management and customer support, ensuring smooth operations for a programming bootcamp. As the sun set, I transformed into a dedicated musician, playing bass and performing with my band.</p>
+            <p>🎸 By day, I excelled in management and customer support, ensuring smooth operations for a programming bootcamp. By hours, I transformed into a dedicated musician, playing bass and performing with my band.</p>
             <p>With a background in managing customer support and a knack for problem-solving and communication, I decided to take on a new challenge. After completing a programming bootcamp myself, I am now looking to transition into a career in development.</p>
             <p>Equipped with hands-on coding experience and a strong foundation in business acumen, I'm eager to bring my unique skills to the tech world.</p>
             <p>Let's create innovative solutions and achieve new successes together – both in the digital realm and beyond!</p>
